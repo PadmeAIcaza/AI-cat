c0566e442600c02c8bcf481da024331a406c0c74
spicypusheen <padme.garrapata@gmail.com>
2026-07-15 17:03:00 -0500

    Chat Bubble functionality

--- src/AIchat.py
+++ src/AIchat.py
@@ -3,13 +3,48 @@ from tkinter import messagebox
 from PIL import Image, ImageTk
 
 class AI:
-    def __init__(self, window):
+    def __init__(self, window, cat):
         self.window = window
+        self.cat = cat
         self.chat_window = None
 
     def open_chat(self, _):
-        BG = '#B34A44'
+        BG = '#E05454'
+        if self.chat_window is not None and self.chst_window.winfo_exists():
+            self.chat_window.lift()
+            return
         self.chat_window = tk.Toplevel(self.window)
-        self.chat_window.configure(bg=BG)
-        self.chat_window.title('Talk With Me!')
-        self.chat_window.geometry('220x180')
+        self.chat_window.title("Talk With Me!")
+        self.chat_window.overrideredirect(True)
+        self.chat_window.attributes("-topmost", True)
+        transparent_color = 'magenta'
+        self.chat_window.configure(bg=transparent_color)
+        self.chat_window.attributes('-transparentcolor', transparent_color)
+        self.chat_window.geometry('220x220')
+
+        self.x = self.chat_window.winfo_screenwidth() - 220
+        self.y = 64
+        self.chat_window.geometry(f"220x220+{self.x}+{self.y}")
+
+        chat_bg = Image.open("../assets/chat.png").resize((220, 220), Image.Resampling.LANCZOS)
+        self.chat_bg = ImageTk.PhotoImage(chat_bg)
+        tk.Label(self.chat_window, image= self.chat_bg, text='Hi!🐱 What can I help you with?', font=("Times New Roman", 10), compound="center", bg='magenta').place(x=0, y=0)
+        chat_entry = tk.Entry(self.chat_window, width=20, font=("Times New Roman", 10))
+        chat_entry.place(x=50, y=130)
+
+        def save_button_pressed():
+            chat_text = chat_entry.get().strip()
+            if chat_text == '':
+                messagebox.showerror(title='Empty Fields', message='Please fill every field')
+                return
+            elif chat_text:
+                print(chat_text)
+                chat_entry.delete(0, tk.END)
+
+        button_image = Image.open('../assets/button.png').resize((50, 20), Image.Resampling.LANCZOS)
+        self.button = ImageTk.PhotoImage(button_image)
+        save_button = tk.Button(self.chat_window, image=self.button, text='Talk', compound='center', fg='black', font=("Times New Roman", 10, "bold"), bd=0, highlightthickness=0, bg=BG, activebackground=BG, command=save_button_pressed)
+        save_button.place(x=85, y=160)
+
+
+
--- src/main.py
+++ src/main.py
@@ -11,19 +11,20 @@ sleep_path = Path('../assets')/'sprites'/'Sleep.png'
 jump_path = Path('../assets')/'sprites'/'Jump.png'
 
 window = tk.Tk()
-AI_bot = AI(window)
+cat = Cat(window, born_path, idle_path, talk_path, sleep_path, jump_path)
+AI_bot = AI(window, cat)
 screen_width = window.winfo_screenwidth()  # 1536
 screen_height = window.winfo_screenheight() # 864
 
-window.title = 'VoidCat'
+window.title("VoidCat")
 window.overrideredirect(True)
 window.attributes("-topmost", True)
 transparent_color = 'magenta'
 window.configure(bg=transparent_color)
 window.attributes('-transparentcolor', transparent_color)
 
-cat = Cat(window, born_path, idle_path, talk_path, sleep_path, jump_path)
 window.geometry(f"{cat.window_width}x{cat.window_height}+{cat.x}+{cat.y}")
+
 cat.label.bind('<Enter>', cat.wake_up)
 cat.label.bind("<Button-1>", AI_bot.open_chat)
 cat.check_idle()
